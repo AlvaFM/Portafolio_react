da7816d5822f1babe82c456c2cf0d7b58530baa4
Alvaro Flores <alv.floresm@duocuc.cl>
2025-08-16 23:54:03 -0400

    actualizacion del portafolio: modificaciones 2

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Inicio from './pages/Inicio';
 import Proyectos from './pages/Proyectos';
 import Aboutme from './pages/Aboutme';
-import Error404 from './pages/404notfound'; // renombrado el componente
+import Error404 from './pages/404notfound';
 import './App.css';
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
-      <BrowserRouter basename="/Portafolio_react">
+      <HashRouter>
         <main className="flex-grow">
           <div className="min-h-screen">
             <Routes>
               <Route path="/" element={<Inicio />} />
               <Route path="/proyectos" element={<Proyectos />} />
               <Route path="/Aboutme" element={<Aboutme />} />
-              <Route path="/*" element={<Error404 />} /> 
+              <Route path="*" element={<Error404 />} />
             </Routes>
           </div>
         </main>
         <Navbar />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
